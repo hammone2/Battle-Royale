f831b5c752d5b20bc233ae13692c49ea9136a010
hammone2 <hammone2@my.erau.edu>
2024-09-19 19:58:39 -0700

    Finished Part 1

---
+++ Battle Royale/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    public int maxPlayers = 10;
+    // instance
+    public static NetworkManager instance;
+    void Awake()
+    {
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // connect to the master server
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("We've connected to the master server!");
+        // seems this is now required in order to receive OnRoomListUpdate callbacks:
+        PhotonNetwork.JoinLobby();
+    }
+
+    // attempts to create a room
+    public void CreateRoom(string roomName)
+    {
+        RoomOptions options = new RoomOptions();
+        options.MaxPlayers = (byte)maxPlayers;
+        PhotonNetwork.CreateRoom(roomName, options);
+    }
+
+    // attempts to join a room
+    public void JoinRoom(string roomName)
+    {
+        PhotonNetwork.JoinRoom(roomName);
+    }
+
+    [PunRPC]
+    public void ChangeScene(string sceneName)
+    {
+        PhotonNetwork.LoadLevel(sceneName);
+    }
+
+
+}
